09e133bde931a99fa4bf2a7bfa386fbec0922528
Markov500 <markonolitse@gmail.com>
2023-04-11 23:01:23 +0000

    projet généré

---
+++ src/main/java/com/example/egaAPI/EgaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.egaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/egaAPI/EgaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
