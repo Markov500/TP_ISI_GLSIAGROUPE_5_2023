1c845a9c7e236b22541b0f74d923a92a1db94f52
RalphKivi <raoulkivi24@gmail.com>
2023-04-14 09:34:22 +0200

    CRUD CLIENT

--- src/main/java/com/example/egaAPI/Controller/ClientController.java
+++ src/main/java/com/example/egaAPI/Controller/ClientController.java
@@ -4,8 +4,60 @@ import com.example.egaAPI.Services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.egaAPI.Entity.Client;
+import com.example.egaAPI.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
+
     @Autowired
-    ClientService clientService;
+    private ClientService clientService;
+
+    // Opération de création d'un client
+    @PostMapping
+    public ResponseEntity<Client> createClient(@RequestBody Client client) {
+        Client newClient = clientService.createClient(client);
+        return new ResponseEntity<>(newClient, HttpStatus.CREATED);
+    }
+
+    // Opération de lecture d'un client par ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClientById(@PathVariable Integer id) {
+        Optional<Client> client = clientService.getClientById(id);
+        return client.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    // Opération de lecture de tous les clients
+    @GetMapping
+    public ResponseEntity<List<Client>> getAllClients() {
+        List<Client> clients = clientService.getAllClients();
+        return new ResponseEntity<>(clients, HttpStatus.OK);
+    }
+
+    // Opération de mise à jour d'un client
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> updateClient(@PathVariable Integer id, @RequestBody Client client) {
+        Client updatedClient = clientService.updateClient(id, client);
+        if (updatedClient != null) {
+            return new ResponseEntity<>(updatedClient, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Opération de suppression d'un client
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteClient(@PathVariable Integer id) {
+        clientService.deleteClient(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
