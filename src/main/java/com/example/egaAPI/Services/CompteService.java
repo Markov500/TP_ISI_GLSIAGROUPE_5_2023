912bb92032e5d69e093f9f7e578649dec610510a
kevin <koussougboss@gmail.com>
2023-04-16 21:53:46 +0000

    CRUD du compte

--- src/main/java/com/example/egaAPI/Services/CompteService.java
+++ src/main/java/com/example/egaAPI/Services/CompteService.java
@@ -1,13 +1,35 @@
 package com.example.egaAPI.Services;
 
-
-import com.example.egaAPI.Repository.ClientRepository;
+import com.example.egaAPI.Entity.Compte;
 import com.example.egaAPI.Repository.CompteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CompteService {
     @Autowired
-    CompteRepository compteRepository;
+    private CompteRepository compteRepository;
+
+    // Opération de création d'un compte
+    public Compte createCompte(Compte compte) {
+        return compteRepository.save(compte);
+    }
+
+    // Opération de lecture d'un compte par ID
+    public Optional<Compte> getCompteById(String numCompte) {
+        return compteRepository.findById(numCompte);
+    }
+
+    // Opération de lecture de tous les comptes
+    public List<Compte> getAllComptes() {
+        return compteRepository.findAll();
+    }
+
+    // Opération de suppression d'un compte
+    public void deleteCompte(String numCompte) {
+        compteRepository.deleteById(numCompte);
+    }
 }
