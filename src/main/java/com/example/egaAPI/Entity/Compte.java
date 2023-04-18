0780b3693bdb9b89b9a466e39f6122970478fc8f
Markov500 <markonolitse@gmail.com>
2023-04-18 08:20:59 +0000

    ajout de l'enregistrement automatique de la date de création

--- src/main/java/com/example/egaAPI/Entity/Compte.java
+++ src/main/java/com/example/egaAPI/Entity/Compte.java
@@ -46,6 +46,7 @@ public class Compte {
 
     public Compte() {
         this.numCompte = _genererNumCompte();
+        this.dateCreation = LocalDate.now();
     }
 
 
@@ -59,7 +60,7 @@ public class Compte {
         this.typeCompte = typeCompte;
     }
 
-   
+
 
 
     public void setClient(Client client) {
